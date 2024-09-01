3bd337e53788302a3ea26d2080d8b98648dc4467
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-09-01 19:34:06 +0100

    adding create contract function

---
+++ src/main/java/ma/wora/impl/ContractRepositoryImpl.java
@@ -0,0 +1,4 @@
+package main.java.ma.wora.impl;
+
+public class ContractRepositoryImpl {
+}
---
+++ src/main/java/ma/wora/presentation/contract/ContractMenu.java
@@ -0,0 +1,17 @@
+package main.java.ma.wora.presentation.contract;
+
+public class ContractMenu {
+    public void displayMenu() {
+        System.out.println("\n--- contract Management System ---");
+        System.out.println("1. Display all contracts");
+        System.out.println("2. Display contract by name");
+        System.out.println("3. Insert contract");
+        System.out.println("4. Update contract");
+        System.out.println("4. Update contract");
+        System.out.println("5. remove contract");
+        System.out.println("6. change status of contract");
+
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
+    }
+}
